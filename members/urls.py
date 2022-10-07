c5574bd2001ff9045c90e85e988153bc2abbea6f
Anderson Rodrigues <eusouanderson@outlook.com>
2022-10-07 10:48:53 -0300

    Heroku Push

---
+++ members/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+  path('', views.initialPag, name='index3.html'),
+  path('about.html', views.about, name='about.html'),
+  path('fruit.html', views.fruit, name='fruit.html'),
+  path('testimonial.html', views.testimonial, name='testimonial.html'),
+  path('contact.html', views.contact, name='contact.html'),
+  path('login.html', views.login, name='login.html'),
+
+  path('index.html/', views.index, name='index.html'),
+  path('index.html/add/', views.add, name='add'),
+  path('index.html/add/addrecord/', views.addrecord, name='addrecord'),
+  path('index.html/delete/<int:id>', views.delete, name='delete'),
+  path('index.html/update/<int:id>', views.update, name='update'),
+
+]
---
+++ members/views.py
@@ -0,0 +1,67 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Members
+
+
+def initialPag(request):
+    template = loader.get_template('index3.html')
+    return HttpResponse(template.render({}, request))
+
+
+def about(request):
+    about = loader.get_template('about.html')
+    return HttpResponse(about.render({}, request))
+
+
+def fruit(request):
+    fruit = loader.get_template('fruit.html')
+    return HttpResponse(fruit.render({}, request))
+
+
+def testimonial(request):
+    testimonial = loader.get_template('testimonial.html')
+    return HttpResponse(testimonial.render({}, request))
+
+def contact(request):
+    contact = loader.get_template('contact.html')
+    return HttpResponse(contact.render({}, request))
+
+def login(request):
+    login = loader.get_template('login.html')
+    return HttpResponse(login.render({}, request))
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
+
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    first = request.POST['first']
+    last = request.POST['last']
+
+    member = Members(firstname=first, lastname=last)
+    member.save()
+
+    return HttpResponseRedirect(reverse('index.html'))
+
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index.html'))
+
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
