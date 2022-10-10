646457aeef0dd92b1da54b440b3f817ed06d9137
Anderson Rodrigues <eusouanderson@outlook.com>
2022-10-10 07:31:14 -0300

    Free HTML TEMPLATES in DJANGO

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn myworld.wsgi --log-file -
-
--- members/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Members(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    firstlogin = models.CharField(max_length=255)
-    userlogin = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
